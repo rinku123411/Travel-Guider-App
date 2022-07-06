fe992c4561c303b4216262adf962e66a718f4957
rinku123411 <rinkudas123411@gmail.com>
2022-07-06 11:24:36 +0530

    connected the sign in page to firebase

--- app/src/main/java/com/example/travelguider/activities/SignInActivity.kt
+++ app/src/main/java/com/example/travelguider/activities/SignInActivity.kt
@@ -1,12 +1,22 @@
 package com.example.travelguider.activities
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import android.view.WindowManager
+import android.widget.Toast
 import com.example.travelguider.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class SignInActivity : AppCompatActivity() {
+class SignInActivity : BaseActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
@@ -14,7 +24,14 @@ class SignInActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+        auth = Firebase.auth
+
         setupActionBar()
+
+        signin_btn.setOnClickListener{
+            signInRegisteredUser()
+        }
+
     }
     private fun setupActionBar(){
         setSupportActionBar(toolbar_sign_in_activity)
@@ -24,5 +41,44 @@ class SignInActivity : AppCompatActivity() {
             actionBar.setHomeAsUpIndicator(R.drawable.back_button)
         }
         toolbar_sign_in_activity.setNavigationOnClickListener{onBackPressed()}
+    }
+    private fun signInRegisteredUser(){
+        val email:String=signin_email.text.toString().trim{it<=' '}
+        val password:String=signin_password.text.toString()
+        if(validateForm(email,password)){
+            showProgressDialog()
+                auth.signInWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(this) { task ->hideProgressDialog()
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCustomToken:success")
+                            startActivity(Intent(this,MainActivity::class.java))
+                            val user = auth.currentUser
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCustomToken:failure", task.exception)
+                            Toast.makeText(baseContext, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+
+        }
+
+    private fun validateForm(email:String,Password:String):Boolean{
+        return when{
+
+            TextUtils.isEmpty(email)->{
+                showErrorSnackBar("Please Enter the email")
+                false
+            }
+            TextUtils.isEmpty(Password)->{
+                showErrorSnackBar("Please Enter the Password")
+                false
+            }
+            else ->{
+                true
+            }
+        }
     }
 }
